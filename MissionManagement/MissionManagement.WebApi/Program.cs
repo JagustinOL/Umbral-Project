097175861b1c9c835f0d5c2840a074a4bb3c2969
José Ojeda <jagustin2907@gmail.com>
2026-06-04 20:55:34 -0400

    Solucion de cors con expo

--- MissionManagement/MissionManagement.WebApi/Program.cs
+++ MissionManagement/MissionManagement.WebApi/Program.cs
@@ -18,16 +18,28 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy(frontendCorsPolicy, policy =>
     {
-        policy
-            .WithOrigins(
+        policy.AllowAnyHeader().AllowAnyMethod();
+
+        if (builder.Environment.IsDevelopment())
+        {
+            policy.SetIsOriginAllowed(origin =>
+            {
+                if (!Uri.TryCreate(origin, UriKind.Absolute, out var uri))
+                    return false;
+                return uri.Scheme == "http"
+                    && (uri.Host is "localhost" or "127.0.0.1");
+            });
+        }
+        else
+        {
+            policy.WithOrigins(
                 "http://localhost:3000",
                 "http://localhost:3001",
                 "http://localhost:3002",
                 "http://localhost:5173",
                 "http://localhost:19000",
-                "http://localhost:19001")
-            .AllowAnyHeader()
-            .AllowAnyMethod();
+                "http://localhost:19001");
+        }
     });
 });
 
